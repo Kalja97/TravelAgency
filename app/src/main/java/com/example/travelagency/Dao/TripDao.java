c06ce7f382233fc8c8bbce2766aa07c8c51a9c76
damianwasmer <damian.wasmer@students.hevs.ch>
2019-03-06 14:55:31 +0100

    DAO und Entity

---
+++ app/src/main/java/com/example/travelagency/Dao/TripDao.java
@@ -0,0 +1,11 @@
+package com.example.travelagency.Dao;
+
+public interface TripDao {
+
+    //@Query("SELECT * FROM trip")
+    //List<Trip> getAll();
+
+    //@Query("Select * FROM trip WHERE location IN (:countryname)")
+    //Trip trip(String countryname);
+
+}
---
+++ app/src/main/java/com/example/travelagency/Entities/Trip.java
@@ -0,0 +1,69 @@
+package com.example.travelagency.Entities;
+
+public class Trip {
+
+    public int id;
+
+    public String location;
+    public String tripname;
+    public String duration;
+    public String date;
+    public String price;
+    public String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTripname() {
+        return tripname;
+    }
+
+    public void setTripname(String tripname) {
+        this.tripname = tripname;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
