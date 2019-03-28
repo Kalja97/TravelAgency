5a6963981c453f7c8597ab6fc13256c4f9c3b461
damianwasmer <damian.wasmer@students.hevs.ch>
2019-03-28 22:37:56 +0100

    Animation Startseite

--- app/src/main/java/com/example/travelagency/ui/Activities/StartActivity.java
+++ app/src/main/java/com/example/travelagency/ui/Activities/StartActivity.java
@@ -4,16 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.travelagency.R;
 
 public class StartActivity extends AppCompatActivity {
 
+    ImageView bgstart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        bgstart = (ImageView) findViewById(R.id.bgstart);
+
+        bgstart.animate().scaleX(1.5f).scaleY(1.5f).setDuration(6000).start();
     }
 
     public void gotomain (View view){
