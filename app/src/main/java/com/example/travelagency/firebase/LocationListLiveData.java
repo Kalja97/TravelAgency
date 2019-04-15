050e1f88c8db65c5dddf7921301bab5b29a5e982
damianwasmer <damian.wasmer@students.hevs.ch>
2019-04-15 08:52:45 +0200

    Firebase LiveData Files komplett

---
+++ app/src/main/java/com/example/travelagency/firebase/LocationListLiveData.java
@@ -0,0 +1,61 @@
+package com.example.travelagency.firebase;
+
+
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.example.travelagency.Entities.LocationF;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocationListLiveData extends LiveData<List<LocationF>> {
+
+    private static final String TAG = "TripListLiveData";
+
+
+    /*
+    private final DatabaseReference reference;
+    private final MyValueEventListener listener = new MyValueEventListener();
+
+    public LocationListLiveData(DatabaseReference ref, String countryName) {
+        reference = ref;
+    }
+
+    @Override
+    protected void onActive() {
+        Log.d(TAG, "onActive");
+        reference.addValueEventListener(listener);
+    }
+
+    @Override
+    protected void onInactive() {
+        Log.d(TAG, "onInactive");
+    }
+
+    private class MyValueEventListener implements ValueEventListener {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            setValue(toLocations(dataSnapshot));
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+            Log.e(TAG, "Can't listen to query " + reference, databaseError.toException());
+        }
+    }
+
+    private List<LocationF> toLocations(DataSnapshot snapshot) {
+        List<LocationF> locations = new ArrayList<>();
+        for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+            LocationF entity = childSnapshot.getValue(LocationF.class);
+            //entity.setId(childSnapshot.getKey());
+            //entity.setCountryName(countryName);
+            locations.add(entity);
+        }
+        return locations;
+    }
+    */
+
+}
---
+++ app/src/main/java/com/example/travelagency/firebase/TripListLiveData.java
@@ -0,0 +1,60 @@
+package com.example.travelagency.firebase;
+
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.example.travelagency.Entities.TripF;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TripListLiveData extends LiveData<List<TripF>> {
+
+    private static final String TAG = "TripListLiveData";
+
+    /*
+    private final DatabaseReference reference;
+    private final String countryName;
+    private final MyValueEventListener listener = new MyValueEventListener();
+
+    public TripListLiveData(DatabaseReference ref, String countryName) {
+        reference = ref;
+        this.countryName = countryName;
+    }
+
+    @Override
+    protected void onActive() {
+        Log.d(TAG, "onActive");
+        reference.addValueEventListener(listener);
+    }
+
+    @Override
+    protected void onInactive() {
+        Log.d(TAG, "onInactive");
+    }
+
+    private class MyValueEventListener implements ValueEventListener {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            setValue(toTrips(dataSnapshot));
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+            Log.e(TAG, "Can't listen to query " + reference, databaseError.toException());
+        }
+    }
+
+    private List<TripF> toTrips(DataSnapshot snapshot) {
+        List<TripF> trips = new ArrayList<>();
+        for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+            TripF entity = childSnapshot.getValue(TripF.class);
+            entity.setId(childSnapshot.getKey());
+            entity.setCountryName(countryName);
+            trips.add(entity);
+        }
+        return trips;
+    }
+    */
+}
