75e2f74eec692d4106df26df1f0c98837df1a8ce
Kalja97 <jannis.kalbermatten@students.hevs.ch>
2019-04-24 10:27:18 +0200

    cloud messaging

--- app/src/main/java/com/example/travelagency/ui/Activities/StartActivity.java
+++ app/src/main/java/com/example/travelagency/ui/Activities/StartActivity.java
@@ -1,23 +1,51 @@
 package com.example.travelagency.ui.Activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.travelagency.R;
 import com.example.travelagency.ui.Activities.location.LocationsActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class StartActivity extends AppCompatActivity {
 
     ImageView bgstart;
+    private final String TAG = "Test";
 
     //on create method
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        //Get the registration token
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        String msg = "Token:" + token;
+                        Log.d(TAG, msg);
+                        Toast.makeText(StartActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
 
         //zooming picture
         bgstart = (ImageView) findViewById(R.id.bgstart);
@@ -29,5 +57,7 @@ public class StartActivity extends AppCompatActivity {
         Intent intent = new Intent (this, LocationsActivity.class);
         startActivity(intent);
     }
+
+
 
 }
