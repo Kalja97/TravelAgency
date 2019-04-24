e86fe7105aab5b8403b1d5143fb578f740d8cb26
damianwasmer <damian.wasmer@students.hevs.ch>
2019-04-24 15:01:28 +0200

    Cloud Messaging

---
+++ app/src/main/java/com/example/travelagency/Firebase/MyFirebaseInstanceIDService.java
@@ -0,0 +1,75 @@
+package com.example.travelagency.Firebase;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.util.Objects;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+
+
+    Context mContext = this;
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+
+
+    private String loginToken = "";
+
+
+
+    @Override
+
+    public void onTokenRefresh() {
+
+        // Get updated InstanceID token.
+
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+
+
+        // If you want to send messages to this application instance or
+
+        // manage this apps subscriptions on the server side, send the
+
+        // Instance ID token to your app server.
+
+        registerNotificationsToken(refreshedToken, loginToken);
+
+    }
+
+
+
+
+
+    private void registerNotificationsToken(String notificationsToken, String loginToken){
+
+
+
+        // Get Token from Shared Pref
+
+
+
+        if(Objects.equals(loginToken, ""))
+
+            return;
+
+    }
+
+
+
+    public void updateToken(String token){
+
+        loginToken = token;
+
+        onTokenRefresh();
+
+    }
+
+}
---
+++ app/src/main/java/com/example/travelagency/Firebase/MyFirebaseMessagingService.java
@@ -0,0 +1,129 @@
+package com.example.travelagency.Firebase;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.example.travelagency.R;
+import com.example.travelagency.ui.Activities.StartActivity;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirbaseMsgService";
+
+
+
+    @Override
+
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        super.onMessageReceived(remoteMessage);
+
+        Log.d(TAG, "FROM:" + remoteMessage.getFrom());
+
+
+
+        // check if the message contains data
+
+        if(remoteMessage.getData().size() > 0){
+
+            Log.d(TAG, "FROM:" + remoteMessage.getData());
+
+        }
+
+
+
+        // check if the message contains notifications
+
+        if(remoteMessage.getNotification() != null){
+
+            Log.d(TAG, "Message Body:" + remoteMessage.getNotification().getBody());
+
+            sendNotification(remoteMessage.getNotification().getBody());
+
+        }
+
+    }
+
+
+
+    /**
+
+     * Create and show a simple notification containing the received FCM message.
+
+     *
+
+     * @param messageBody FCM message body received.
+
+     */
+
+    private void sendNotification(String messageBody) {
+
+        Intent intent = new Intent(this, StartActivity.class);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+
+                PendingIntent.FLAG_ONE_SHOT);
+
+
+
+        String channelId = getString(R.string.app_name);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder =
+
+                new NotificationCompat.Builder(this, channelId)
+
+                        .setSmallIcon(R.drawable.ic_info_black_24dp)
+
+                        .setContentTitle("FCM Message")
+
+                        .setContentText(messageBody)
+
+                        .setAutoCancel(true)
+
+                        .setSound(defaultSoundUri)
+
+                        .setContentIntent(pendingIntent);
+
+
+
+        NotificationManager notificationManager =
+
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+
+        // Since android Oreo notification channel is needed.
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            NotificationChannel channel = new NotificationChannel(channelId,
+
+                    "Channel human readable title",
+
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationManager.createNotificationChannel(channel);
+
+        }
+
+
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+
+    }
+
+}
